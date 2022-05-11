5f7bd70deb4789d37581a2d5a7d7c7aa79f34dd2
tod91 <todorivanov066@gmail.com>
2022-05-11 18:17:01 +0300

    added some openGL code and user input

--- main.go
+++ main.go
@@ -1,12 +1,20 @@
 package main
 
 import (
+	"github.com/go-gl/gl/v3.3-core/gl"
 	"github.com/go-gl/glfw/v3.3/glfw"
+	"log"
 	"runtime"
 )
 
 func init() {
 	runtime.LockOSThread()
+}
+
+func processInput(window *glfw.Window) {
+	if window.GetKey(glfw.KeyEscape) == glfw.Press {
+		window.SetShouldClose(true)
+	}
 }
 
 func main() {
@@ -23,7 +31,17 @@ func main() {
 
 	window.MakeContextCurrent()
 
+	if err := gl.Init(); err != nil {
+		log.Fatalln(err)
+	}
+
+	gl.Viewport(0, 0, 640, 480)
 	for !window.ShouldClose() {
+
+		processInput(window)
+
+		gl.ClearColor(0.2, 0.3, 0.3, 1.0)
+		gl.Clear(gl.COLOR_BUFFER_BIT)
 		window.SwapBuffers()
 		glfw.PollEvents()
 	}
