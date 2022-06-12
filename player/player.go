ac48ca005d9e5866ae63c7857329aa7de2a022f9
tod91 <todorivanov066@gmail.com>
2022-06-12 20:33:09 +0300

    added turn action interface to player

--- player/player.go
+++ player/player.go
@@ -1,7 +1,29 @@
 package player
 
-type Components struct {
+type Player struct {
 	Cities      uint32
 	Settlements uint32
 	Roads       uint32
+}
+
+type Turn interface {
+	DrawDevCard()
+	RollDice()
+	Build()
+	Trade()
+}
+
+func (p *Player) DrawDevCard() {
+
+}
+
+func (p *Player) RollDice() {
+
+}
+func (p *Player) Build() {
+
+}
+
+func (p *Player) Trade() {
+
 }
