5bc842b6937904cf5a8fa730d2cc64ddee114983
todor.ivanov <todor.ivanov@egt-bg.com>
2022-08-08 12:30:11 +0300

    fixed path to resources

--- resources/resources.go
+++ resources/resources.go
@@ -7,55 +7,55 @@ type tx = texture.Texture2D
 var Get = make(map[string]*tx)
 
 func init() {
-	clay, err := texture.NewTexture2DFromImage("resources/jpegs/clay.jpg")
+	clay, err := texture.NewTexture2DFromImage("resources/pics/clay.jpg")
 	if err != nil {
 		panic(err)
 	}
 	Get["clay"] = clay
 
-	grain, err := texture.NewTexture2DFromImage("resources/jpegs/grain.jpg")
+	grain, err := texture.NewTexture2DFromImage("resources/pics/grain.jpg")
 	if err != nil {
 		panic(err)
 	}
 	Get["grain"] = grain
 
-	rock, err := texture.NewTexture2DFromImage("resources/jpegs/rock.jpg")
+	rock, err := texture.NewTexture2DFromImage("resources/pics/rock.jpg")
 	if err != nil {
 		panic(err)
 	}
 	Get["rock"] = rock
 
-	wood, err := texture.NewTexture2DFromImage("resources/jpegs/wood.jpg")
+	wood, err := texture.NewTexture2DFromImage("resources/pics/wood.jpg")
 	if err != nil {
 		panic(err)
 	}
 	Get["wood"] = wood
 
-	clay_field, err := texture.NewTexture2DFromImage("resources/jpegs/clay_field.png")
+	clay_field, err := texture.NewTexture2DFromImage("resources/pics/clay_field.png")
 	if err != nil {
 		panic(err)
 	}
 	Get["clay_field"] = clay_field
 
-	grain_field, err := texture.NewTexture2DFromImage("resources/jpegs/grain_field.png")
+	grain_field, err := texture.NewTexture2DFromImage("resources/pics/grain_field.png")
 	if err != nil {
 		panic(err)
 	}
 	Get["grain_field"] = grain_field
 
-	rock_field, err := texture.NewTexture2DFromImage("resources/jpegs/rock_field.png")
+	rock_field, err := texture.NewTexture2DFromImage("resources/pics/rock_field.png")
 	if err != nil {
 		panic(err)
 	}
 	Get["rock_field"] = rock_field
 
-	sheep_field, err := texture.NewTexture2DFromImage("resources/jpegs/sheep_field.png")
+	sheep_field, err := texture.NewTexture2DFromImage("resources/pics/sheep_field.png")
 	if err != nil {
 		panic(err)
 	}
 	Get["sheep_field"] = sheep_field
 
-	wood_field, err := texture.NewTexture2DFromImage("resources/jpegs/wood_field.png")
+	wood_field, err := texture.NewTexture2DFromImage("resources/pics/wood_field.png")
 	if err != nil {
 		panic(err)
 	}
