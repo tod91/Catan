69b37f0f36216e499f6604d626b5cb1340b01d98
todor.ivanov <todor.ivanov@egt-bg.com>
2022-08-10 13:49:54 +0300

    moved loading resources to new package

--- main.go
+++ main.go
@@ -1,16 +1,13 @@
 package main
 
 import (
-	"Catan/resources"
+	"Catan/main_game"
 	"github.com/g3n/engine/app"
 	"github.com/g3n/engine/camera"
 	"github.com/g3n/engine/core"
-	"github.com/g3n/engine/geometry"
 	"github.com/g3n/engine/gls"
-	"github.com/g3n/engine/graphic"
 	"github.com/g3n/engine/gui"
 	"github.com/g3n/engine/light"
-	"github.com/g3n/engine/material"
 	"github.com/g3n/engine/math32"
 	"github.com/g3n/engine/renderer"
 	"github.com/g3n/engine/util/helper"
@@ -20,11 +17,11 @@ import (
 
 func main() {
 
-	// Create application and scene
+	// Create application and main_game
 	a := app.App()
 	scene := core.NewNode()
 
-	// Set the scene to be managed by the gui manager
+	// Set the main_game to be managed by the gui manager
 	gui.Manager().Set(scene)
 
 	// Create perspective camera
@@ -46,77 +43,15 @@ func main() {
 	a.Subscribe(window.OnWindowSize, onResize)
 	onResize("", nil)
 
-	geom := geometry.NewPlane(6, 6)
-	mat := material.NewStandard(math32.NewColor("white"))
-	mat.AddTexture(resources.Get["rock_field"])
-	mat.SetBlending(material.BlendNormal)
-	mat.SetSide(material.SideDouble)
-	mesh := graphic.NewMesh(geom, mat)
-	scene.Add(mesh)
+	main_game.LoadScene(scene)
 
-	plane := geometry.NewPlane(4, 6)
-	mat2 := material.NewStandard(math32.NewColor("white"))
-	mat2.AddTexture(resources.Get["clay"])
-	mat2.SetBlending(material.BlendNormal)
-	mat2.SetSide(material.SideDouble)
-	mesh2 := graphic.NewMesh(plane, mat2)
-	mesh2.SetPositionX(6)
-	scene.Add(mesh2)
-
-	//skyboxData := graphic.SkyboxData{DirAndPrefix: "resources/pics/", Suffixes: [6]string{"left", "right", "top", "bottom", "back", "front"}, Extension: "jpg"}
-	//skybox, er := graphic.NewSkybox(skyboxData)
-	//if er != nil {
-	//	panic(er)
-	//}
-	cube := geometry.NewCube(6)
-	vbo := cube.GetGeometry().VBOs()
-	test := vbo[2].Buffer()
-	test.SetVector2(0, &math32.Vector2{X: 0.25, Y: 0.66})
-	test.SetVector2(2, &math32.Vector2{X: 0.25, Y: 0.33})
-	test.SetVector2(4, &math32.Vector2{X: 0, Y: 0.66})
-	test.SetVector2(6, &math32.Vector2{X: 0, Y: 0.33})
-
-	test.SetVector2(8, &math32.Vector2{X: 0.5, Y: 0.66})
-	test.SetVector2(10, &math32.Vector2{X: 0.5, Y: 0.33})
-	test.SetVector2(12, &math32.Vector2{X: 0.25, Y: 0.66})
-	test.SetVector2(14, &math32.Vector2{X: 0.25, Y: 0.33})
-
-	test.SetVector2(16, &math32.Vector2{X: 0.75, Y: 0.66})
-	test.SetVector2(18, &math32.Vector2{X: 0.75, Y: 0.33})
-	test.SetVector2(20, &math32.Vector2{X: 0.5, Y: 0.66})
-	test.SetVector2(22, &math32.Vector2{X: 0.5, Y: 0.33})
-
-	test.SetVector2(24, &math32.Vector2{X: 1, Y: 0.66})
-	test.SetVector2(26, &math32.Vector2{X: 1, Y: 0.33})
-	test.SetVector2(28, &math32.Vector2{X: 0.75, Y: 0.66})
-	test.SetVector2(30, &math32.Vector2{X: 0.75, Y: 0.33})
-
-	test.SetVector2(32, &math32.Vector2{X: 0.5, Y: 1.0})
-	test.SetVector2(34, &math32.Vector2{X: 0.5, Y: 0.66})
-	test.SetVector2(36, &math32.Vector2{X: 0.25, Y: 1.0})
-	test.SetVector2(38, &math32.Vector2{X: 0.25, Y: 0.66})
-
-	test.SetVector2(40, &math32.Vector2{X: 0.5, Y: 0.33})
-	test.SetVector2(42, &math32.Vector2{X: 0.5, Y: 0.0})
-	test.SetVector2(44, &math32.Vector2{X: 0.25, Y: 0.33})
-	test.SetVector2(46, &math32.Vector2{X: 0.25, Y: 0.0})
-
-	mat3 := material.NewStandard(math32.NewColor("white"))
-	mat3.AddTexture(resources.Get["die"])
-	mat3.SetBlending(material.BlendNormal)
-	mat3.SetSide(material.SideFront)
-	mesh3 := graphic.NewMesh(cube, mat3)
-	mesh3.SetPositionX(12)
-
-	scene.Add(mesh3)
-
-	// Create and add lights to the scene
+	// Create and add lights to the main_game
 	scene.Add(light.NewAmbient(&math32.Color{1.0, 1.0, 1.0}, 0.8))
 	pointLight := light.NewPoint(&math32.Color{1, 1, 1}, 5.0)
 	pointLight.SetPosition(1, 0, 2)
 	scene.Add(pointLight)
 
-	// Create and add an axis helper to the scene
+	// Create and add an axis helper to the main_game
 	node := helper.NewAxes(0.5)
 	scene.Add(node)
 
---
+++ main_game/scene.go
@@ -0,0 +1,70 @@
+package main_game
+
+import (
+	"Catan/resources"
+	"github.com/g3n/engine/core"
+	"github.com/g3n/engine/geometry"
+	"github.com/g3n/engine/graphic"
+	"github.com/g3n/engine/material"
+	"github.com/g3n/engine/math32"
+)
+
+func LoadScene(scene *core.Node) {
+	loadTile(scene, "rock_field", 6, 6, 0)
+	loadTile(scene, "clay_field", 6, 6, 6)
+	loadCubeMap(scene, 18)
+}
+
+func loadTile(scene *core.Node, name string, w float32, h float32, posX float32) {
+	geom := geometry.NewPlane(w, h)
+	mat := material.NewStandard(math32.NewColor("white"))
+	mat.AddTexture(resources.Get[name])
+	mat.SetBlending(material.BlendNormal)
+	mat.SetSide(material.SideDouble)
+	mesh := graphic.NewMesh(geom, mat)
+	mesh.SetPositionX(posX)
+	scene.Add(mesh)
+}
+
+func loadCubeMap(scene *core.Node, posX float32) {
+	cube := geometry.NewCube(6)
+	vbo := cube.GetGeometry().VBOs()
+	uvCoords := vbo[2].Buffer()
+	uvCoords.SetVector2(0, &math32.Vector2{X: 0.25, Y: 0.66})
+	uvCoords.SetVector2(2, &math32.Vector2{X: 0.25, Y: 0.33})
+	uvCoords.SetVector2(4, &math32.Vector2{X: 0, Y: 0.66})
+	uvCoords.SetVector2(6, &math32.Vector2{X: 0, Y: 0.33})
+
+	uvCoords.SetVector2(8, &math32.Vector2{X: 0.5, Y: 0.66})
+	uvCoords.SetVector2(10, &math32.Vector2{X: 0.5, Y: 0.33})
+	uvCoords.SetVector2(12, &math32.Vector2{X: 0.25, Y: 0.66})
+	uvCoords.SetVector2(14, &math32.Vector2{X: 0.25, Y: 0.33})
+
+	uvCoords.SetVector2(16, &math32.Vector2{X: 0.75, Y: 0.66})
+	uvCoords.SetVector2(18, &math32.Vector2{X: 0.75, Y: 0.33})
+	uvCoords.SetVector2(20, &math32.Vector2{X: 0.5, Y: 0.66})
+	uvCoords.SetVector2(22, &math32.Vector2{X: 0.5, Y: 0.33})
+
+	uvCoords.SetVector2(24, &math32.Vector2{X: 1, Y: 0.66})
+	uvCoords.SetVector2(26, &math32.Vector2{X: 1, Y: 0.33})
+	uvCoords.SetVector2(28, &math32.Vector2{X: 0.75, Y: 0.66})
+	uvCoords.SetVector2(30, &math32.Vector2{X: 0.75, Y: 0.33})
+
+	uvCoords.SetVector2(32, &math32.Vector2{X: 0.5, Y: 1.0})
+	uvCoords.SetVector2(34, &math32.Vector2{X: 0.5, Y: 0.66})
+	uvCoords.SetVector2(36, &math32.Vector2{X: 0.25, Y: 1.0})
+	uvCoords.SetVector2(38, &math32.Vector2{X: 0.25, Y: 0.66})
+
+	uvCoords.SetVector2(40, &math32.Vector2{X: 0.5, Y: 0.33})
+	uvCoords.SetVector2(42, &math32.Vector2{X: 0.5, Y: 0.0})
+	uvCoords.SetVector2(44, &math32.Vector2{X: 0.25, Y: 0.33})
+	uvCoords.SetVector2(46, &math32.Vector2{X: 0.25, Y: 0.0})
+
+	mat := material.NewStandard(math32.NewColor("white"))
+	mat.AddTexture(resources.Get["die"])
+	mat.SetBlending(material.BlendNormal)
+	mat.SetSide(material.SideFront)
+	mesh3 := graphic.NewMesh(cube, mat)
+	mesh3.SetPositionX(posX)
+	scene.Add(mesh3)
+}
